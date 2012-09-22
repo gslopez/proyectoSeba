4bcf51f295f9afa99f146f8f44546ea08d9c01ff
Gonzalo <gslopez@uc.cl>
2012-09-22 05:35:16 -0300

    Nueva version arreglada

--- cheaper/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f6eda56dae610c3f3ec882b7fee260ef88f8115572ed761f349b890a43dd598c97404b6120200b368a68fc7e2121aea43315e4aa6e45a9115ae99990c1d8ead9';
---
+++ cheaper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheaper::Application.config.secret_token = '3e984e8701bdb7fe14024ee901031ebb85fbcc87e06894fb7b5de0920ba36ec752d279572040edd542f9cfa070f5b063a36b090119a1e5dc31c0c0cbb775fe28'
