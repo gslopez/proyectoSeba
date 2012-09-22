9def6e9bff3b09fd370470871ea89050382b28c8
Gonzalo <gslopez@uc.cl>
2012-09-22 05:43:16 -0300

    version2

---
+++ cheaper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheaper::Application.config.secret_token = '48353666c064a05c2c88a3b5ef3e4adfacb8a84566db92ece2a9f232ab2a9c0b0ffbc9b16661fc2e9255d9e5ce5a6a72642072dd841dcc23ec4744f3091097be'
