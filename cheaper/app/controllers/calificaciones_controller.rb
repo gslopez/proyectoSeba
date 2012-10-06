922759daf49380ec623b597d6bf0f10da2b17827
Gonzalo <gslopez@uc.cl>
2012-10-06 01:24:57 -0300

    Servidor funcionando

---
+++ cheaper/app/controllers/calificaciones_controller.rb
@@ -0,0 +1,19 @@
+class CalificacionesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+end
